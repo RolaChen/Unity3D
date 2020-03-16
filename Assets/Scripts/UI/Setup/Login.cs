d9bb278734ccddbd5d547a91e19bba1fdf425949
Rola <976950078@qq.com>
2020-03-16 15:46:39 +0800

    test one

---
+++ Assets/Scripts/UI/Setup/Login.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Login : MonoBehaviour
+{
+    private InputField _inputAccount;
+    private InputField _inputPassword;
+    private Button _btnLogin;
+
+    private void Awake()
+    {
+        _inputAccount = transform.Find("InputAccount").GetComponent<InputField>();
+        _inputPassword= transform.Find("InputPassword").GetComponent<InputField>();
+        _btnLogin= transform.Find("BtnLogin").GetComponent<Button>();
+
+        _btnLogin.onClick.AddListener(onBtnLoginClick);
+    }
+
+    private void onBtnLoginClick()
+    {
+        //连接服务器，等待返回数据
+        //暂时用假数据，直接进入选人界面
+        SceneManager.LoadScene("SelectRole");
+
+    }
+}
---
+++ Assets/Scripts/UserData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class SelectRoleInfo
+{
+    public string Name;
+    public string ModelResPath; //模型资源路径
+}
+public static class UserData
+{
+    //假的角色数据
+    public static List<SelectRoleInfo> AllRole = new List<SelectRoleInfo>();
+
+    static UserData()
+    {
+        AllRole.Add(new SelectRoleInfo() { Name = "第一个角色", ModelResPath = "" });
+        AllRole.Add(new SelectRoleInfo() { Name = "第二个角色", ModelResPath = "" });
+        AllRole.Add(new SelectRoleInfo() { Name = "第三个角色", ModelResPath = "" });
+    }
+}
