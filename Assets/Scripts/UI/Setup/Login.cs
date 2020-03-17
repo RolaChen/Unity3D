2aecc0579c7fc1dcef0c4475383bd12d8409fa56
Rola <976950078@qq.com>
2020-03-17 16:15:09 +0800

    login finished

--- Assets/Scripts/UI/Setup/Login.cs
+++ Assets/Scripts/UI/Setup/Login.cs
@@ -10,13 +10,14 @@ public class Login : MonoBehaviour
     private InputField _inputAccount;
     private InputField _inputPassword;
     private Button _btnLogin;
+    private Text _message;
 
     private void Awake()
     {
         _inputAccount = transform.Find("InputAccount").GetComponent<InputField>();
         _inputPassword= transform.Find("InputPassword").GetComponent<InputField>();
         _btnLogin= transform.Find("BtnLogin").GetComponent<Button>();
-
+        _message = transform.Find("Message").GetComponent<Text>();
         _btnLogin.onClick.AddListener(onBtnLoginClick);
     }
 
@@ -24,7 +25,20 @@ public class Login : MonoBehaviour
     {
         //连接服务器，等待返回数据
         //暂时用假数据，直接进入选人界面
-        SceneManager.LoadScene("SelectRole");
+        //SceneManager.LoadScene("SelectRole");
+        //ConnectMySql.instance.SelectWhere("player", new string[] { "*" }, new string[] { "login" },
+        //new string[] { "=" }, new string[] { "1" });
+        //Debug.Log(PlayerPrefs.GetString("name"));
+        //Debug.Log(PlayerPrefs.GetString("password"));
+        //Debug.Log(PlayerPrefs.GetString("login"));
+        string address = _inputAccount.text;
+        string password = _inputPassword.text;
+        if (ConnectMySql.instance.login(address, password))
+        {
+            _message.text = "登入成功";
+        }
+        else
+            _message.text = "用户名或密码错误";
 
     }
 }
