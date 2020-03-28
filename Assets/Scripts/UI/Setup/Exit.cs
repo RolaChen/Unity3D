cd8b801a1d778c67bdf400ed7b1720c9c986828f
Rola <976950078@qq.com>
2020-03-28 17:30:24 +0800

    exit regist finish add the login state

---
+++ Assets/Scripts/UI/Setup/Exit.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class Exit : MonoBehaviour
+{
+    private Button _exit;
+    private string url = "http://106.14.161.155/QianZhi/exit.php";
+
+    void Start()
+    {
+        _exit = transform.Find("Exit").GetComponent<Button>();
+        _exit.onClick.AddListener(exit);
+    }
+
+    private void exit()
+    {
+        StartCoroutine(live());
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+
+    IEnumerator live()
+    {
+        WWWForm add = new WWWForm();
+        add.AddField("id", PlayerData.instance.id);
+        UnityWebRequest webRequest = UnityWebRequest.Post(url, add);
+        yield return webRequest.SendWebRequest();
+    }
+
+}
---
+++ Assets/Scripts/UI/Setup/Regist.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Regist : MonoBehaviour
+{
+    private string url = "http://106.14.161.155/QianZhi/regist.php";
+    public GameObject _login;
+    public GameObject _regist;
+    private InputField _name;
+    private InputField _password;
+    private InputField _repassword;
+    private Button _btnBack;
+    private Button _btnRegist;
+    private Text _message;
+
+    void Start()
+    {
+        _name = transform.Find("Name").GetComponent<InputField>();
+        _password = transform.Find("Password").GetComponent<InputField>();
+        _repassword = transform.Find("re_Password").GetComponent<InputField>();
+        _btnBack = transform.Find("Back").GetComponent<Button>();
+        _btnRegist = transform.Find("Regist").GetComponent<Button>();
+        _message = transform.Find("Message").GetComponent<Text>();
+        _btnBack.onClick.AddListener(OnBtnBackClick);
+        _btnRegist.onClick.AddListener(OnBtnRegistClick);
+    }
+
+    private void OnBtnBackClick()
+    {
+        _login.SetActive(true);
+        _regist.SetActive(false);
+    }
+
+    private void OnBtnRegistClick()
+    {
+        if (_password.text != _repassword.text)
+            _message.text = "两次密码不一致";
+        else
+        {
+            StartCoroutine(regist(_name.text, _password.text));
+        }         
+    }
+
+    IEnumerator regist(string name, string password)
+    {
+        WWWForm add = new WWWForm();
+        add.AddField("name", name);
+        add.AddField("password", password);
+        UnityWebRequest webRequest = UnityWebRequest.Post(url, add);
+        yield return webRequest.SendWebRequest();
+        if (webRequest.isHttpError || webRequest.isNetworkError)
+            Debug.Log(webRequest.error);
+        else
+        {
+            string information = webRequest.downloadHandler.text.ToString();
+            //Debug.Log(information);
+            string[] get = information.Split('%');
+            if (get[0] == "succeed")
+            {
+                PlayerData.instance.name = name;
+                PlayerData.instance.id = get[1];
+                PlayerPrefs.SetString("name", name);
+                PlayerPrefs.SetString("password", password);
+                SceneManager.LoadScene("SelectRole");
+            }
+            else
+                _message.text = get[0];
+        }
+    }
+}
