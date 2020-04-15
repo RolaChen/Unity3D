62f9652e2bbf410c93b28b300e302a966a243bf8
Rola <976950078@qq.com>
2020-04-16 00:40:07 +0800

    加了进入房子的碰撞器

---
+++ Assets/Scripts/UI/EnterNewScene.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterNewScene : MonoBehaviour
+{
+    public Transform player;
+    bool m_IsPlayerInRange;
+    public GameObject image;
+    // Start is called before the first frame update
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform==player)
+        {
+            m_IsPlayerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.transform==player)
+        {
+            m_IsPlayerInRange = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_IsPlayerInRange)
+            image.SetActive(true);
+        else
+            image.SetActive(false);
+    }
+}
